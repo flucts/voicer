300d9258541b04791236561ac7efc1ddefe6ee80
flucts <fluctss@gmail.com>
2020-10-07 22:42:27 -0300

    Updates

---
+++ precache-manifest.27486e3e97ffcec4fdb93ed8ad6dbc72.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6c1a844197f271b5565ad0e18d534dd0",
+    "url": "/voicer/index.html"
+  },
+  {
+    "revision": "8a48f19e5faf797a57b2",
+    "url": "/voicer/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "90e52c57d37746d906fc",
+    "url": "/voicer/static/js/2.763b34e5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/voicer/static/js/2.763b34e5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8a48f19e5faf797a57b2",
+    "url": "/voicer/static/js/main.eff130e4.chunk.js"
+  },
+  {
+    "revision": "6fb5af57431560f0a201",
+    "url": "/voicer/static/js/runtime-main.2ca009fd.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/voicer/static/media/logo.5d5d9eef.svg"
+  }
+]);
